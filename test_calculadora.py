cc9e41aa4cae1f4d4bbf5036a687df38bd29d7a6
MUXGAME <muxgame1990@gmail.com>
2026-09-11 20:33:21 -0700

    Agregar prueba unitaria para metodo subtract

--- test_calculadora.py
+++ test_calculadora.py
@@ -2,4 +2,8 @@ from calculadora import Calculadora
 
 def test_add():
     calc = Calculadora()
-    assert calc.add(2,3) == 5    
+    assert calc.add(2,3) == 5
+
+def test_subtract():
+    calc = Calculadora()
+    assert calc.subtract(5,3) == 2    
